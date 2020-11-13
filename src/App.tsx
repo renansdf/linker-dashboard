f22f306cfe6cd7a7bcbf2afe8c682e5b8988fb76
renansdf <rnn.frts@gmail.com>
2020-11-12 23:42:47 -0300

    innitial setup of mockup phone and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
+import GlobalStyles, { MobileContainer, Content, IphoneTop, IphoneRight, IphoneBottom, IphoneLeft } from './styles/GlobalStyles';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MobileContainer>
+        <IphoneTop />
+        <IphoneRight />
+        <IphoneBottom />
+        <IphoneLeft />
+        <Content>
+          <Routes />
+        </Content>
+      </MobileContainer>
+
+      <GlobalStyles />
+    </Router>
   );
 }
 
