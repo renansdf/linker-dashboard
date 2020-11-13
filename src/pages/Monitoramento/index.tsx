1612fd35b1240faac8bf984c1ed6d273e7b5d76c
Anderson Campos <anderleitec@gmail.com>
2020-11-13 20:43:17 -0300

    Chart and monitoring page

--- src/pages/Monitoramento/index.tsx
+++ src/pages/Monitoramento/index.tsx
@@ -1,15 +1,56 @@
 import React from 'react';
-import Menu from '../../components/Menu';
-import PhoneHeader from '../../components/PhoneHeader';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import PhoneHeader from '../../components/PhoneHeader';
+import Chart from '../../components/Chart';
+import LampImg from '../../images/lampada.png'
+
+import { Container, Balance, Text, Insights } from './styles';
 
 const Monitoramento: React.FC = () => {
+  const data = [
+    {week: 1, income: 13000, costs: 10000},
+    {week: 2, income: 16500, costs: 17000},
+    {week: 3, income: 14250, costs: 6000},
+    {week: 4, income: 19000, costs: 12000}
+  ]
+
   return (
     <Container>
-      <PhoneHeader>monitore sua conta</PhoneHeader>
-      <h1>Monitoramento</h1>
-      <Menu />
+      <PhoneHeader>Monitore sua conta</PhoneHeader>
+
+      <Balance>
+        <div>
+          <span>Receitas</span>
+          <span className="income">R$ 1.000.000,00</span>
+        </div>
+        <div>
+          <span>Custos</span>
+          <span className="costs">R$ 300.000,00</span>
+        </div>
+      </Balance>
+
+      <Chart data={data}/>
+
+      <Text>
+        Que tal ler mais sobre dicas para aumentar a receita?
+      </Text>
+
+      <Insights>
+        <div>
+          <span>Como se adaptar à nova realidade?</span>
+        </div>
+        <div>
+          <span>
+            5 dicas para reduzir gastos em tempos de crise
+          </span>
+        </div>
+      </Insights>
+
+      <Link to="/insights">
+        <img src={LampImg} alt="Lampada"/>
+        MAIS INSIGHTS
+      </Link>
     </Container>
   );
 }
--- src/pages/Monitoramento/styles.ts
+++ src/pages/Monitoramento/styles.ts
@@ -1,4 +1,83 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin: 0 16px 16px;
+
+  a {
+    height: 48px;
+    margin-top: 16px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 25px;
+    font-weight: 800;
+    text-decoration: none;
+    color: #fff;
+
+    background: #306292;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+    cursor: default;
+  }
+`;
+
+export const Balance = styled.div`
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+
+  margin-top: 25px;
+  padding: 8px 20px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 10px;
+
+    span {
+      font-size: 18px;
+      font-weight: 600;
+      color: #A1A5AC;
+    }
+
+    span.income {
+      color: #53DC8D;
+    }
+
+    span.costs {
+      color: #666;
+    }
+  }
+`;
+
+export const Text = styled.span`
+  padding: 0 16px;
+
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Insights = styled.div`
+  margin-top: 16px;
+  display: flex;
+  gap: 25px;
+
+  div {
+    width: 148px;
+    background: #EFF7FF;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+
+    padding: 16px 4px 16px 10px;
+
+    span {
+    font-size: 18px;
+    font-weight: 600;
+    color: #53575D;
+    }
+  } 
 `;
