bac03990dfe4269e1c34c47f879d75ae456bc77f
gabrielcancio <gabriel.cancio02@gmail.com>
2020-11-14 18:22:57 -0300

    feature/tag_screen

--- src/pages/Tag/index.tsx
+++ src/pages/Tag/index.tsx
@@ -1,15 +1,118 @@
-import React from 'react';
-import Menu from '../../components/Menu';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiHelpCircle } from 'react-icons/fi';
 import PhoneHeader from '../../components/PhoneHeader';
 
-import { Container } from './styles';
+import { Container, Content, CardBlock, Card, Input, Span, TagsBlock, ButtonBlock, Button } from './styles';
 
 const Tag: React.FC = () => {
+  const [selectedCard, setSelectedCard] = useState<number>(1);
+  const [selectedTag, setSelectedTag] = useState<number>(1);
+  const [value, setValue] = useState<number>();
+
+  const history = useHistory();
+
+  function handleSubmit() {
+    const data = new FormData;
+
+    data.append('value', String(value));
+    data.append('tag_type', String(selectedCard)); // Entrada ou Saída
+    data.append('tag', String(selectedTag));
+
+    // ENVIAR PARA O SERVER
+    history.push('/dashboard');
+  }
+
+  function handleCancel(){
+    history.push('/dashboard');
+  }
+
+  function handleSelectTag(tag: number) {
+    setSelectedTag(tag);
+  }
+
+  function handleSelectCard(card: number) {
+    setSelectedCard(card);
+  }
+
   return (
     <Container>
-      <PhoneHeader>tag</PhoneHeader>
-      <h1>Tag</h1>
-      <Menu />
+      <PhoneHeader 
+        color="#306292" 
+        background="#F0F0F0"
+      >
+        Taggeamento
+      </PhoneHeader>
+
+      <Content >
+        <span>Como você deseja<br/>taggear esta transação?</span>
+
+        <CardBlock>
+          <Card 
+            className={selectedCard === 1 ? 'selected-card' : '' } 
+            onClick={() => handleSelectCard(1)
+          }>
+            <p>Entrada</p>
+          </Card>
+          <Card
+            className={selectedCard === 2 ? 'selected-card' : ''} 
+            onClick={() => handleSelectCard(2)}
+          >
+            <p>Saída</p>
+          </Card>
+        </CardBlock>
+
+        <span>Valor da transação:</span>
+        <Input
+          type="number"
+          placeholder="R$ 2500,00"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+
+        <Span>
+          Tags:  
+          <FiHelpCircle className="help-image" size={22} color="#B6B6B6"/>
+        </Span>
+
+        <TagsBlock>
+          <div 
+            className={ selectedTag === 1 ? 'tag-selected' : '' }
+            onClick={() => handleSelectTag(1)}
+          >
+            Aporte
+          </div>
+          <div 
+            className={ selectedTag === 2 ? 'tag-selected' : '' }
+            onClick={() => handleSelectTag(2)}
+          >
+            Custo fixo
+          </div>
+          <div 
+            className={ selectedTag === 3 ? 'tag-selected' : '' }
+            onClick={() => handleSelectTag(3)}
+          >
+            Custo variável
+          </div>
+          <div 
+            className={ selectedTag === 4 ? 'tag-selected' : '' }
+            onClick={() => handleSelectTag(4)}
+          >
+            Receita bruta
+          </div>
+          <div 
+            className={ selectedTag === 5 ? 'tag-selected' : '' }
+            onClick={() => handleSelectTag(5)}
+          >
+            Receita líquida
+          </div>
+        </TagsBlock>
+
+        <ButtonBlock>
+          <Button onClick={handleCancel} background="#FA6D6C">Cancelar</Button>
+          <Button onClick={handleSubmit} background="#306292">Confirmar</Button>
+        </ButtonBlock>
+      </Content>
     </Container>
   );
 }
--- src/pages/Tag/styles.ts
+++ src/pages/Tag/styles.ts
@@ -1,4 +1,153 @@
 import styled from 'styled-components';
 
+interface IButtonProps {
+  background: string;
+}
+
 export const Container = styled.div`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+
+  input[type=number]::-webkit-inner-spin-button,
+  input[type=number]::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+  }
+`;
+
+export const Content = styled.div`
+  padding: 0 28px;
+`;
+
+export const CardBlock = styled.div`
+  width: 100%;
+  height: 86px;
+  margin-top: 18px;
+  margin-bottom: 29px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .selected-card {
+    border: 3px solid #FA6D6C;
+  }
+`;
+
+export const Card = styled.div`
+  width: 145px;
+  height: 86px;
+  padding: 8px 13px;
+
+  background: #FFFFFF;
+  box-sizing: border-box;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  cursor: pointer;
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: right;
+
+  p {
+    color: #306292;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 20px;
+  }
+`;
+
+export const Input = styled.input`
+  width: 318px;
+  height: 49px;
+  margin-top: 15px;
+  padding: 15px 13px;
+
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+
+
+  background: #FFFFFF;
+  border: 1px solid #ACACAC;
+  box-sizing: border-box;
+  border-radius: 8px;
+
+  -webkit-appearance: none;
+`;
+
+export const Span = styled.span`
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+
+  .help-image {
+    margin-right: 5px;
+  }
+`;
+
+export const TagsBlock = styled.div`
+  margin-top: 21px;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+
+  div {
+    height: 44px;
+
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 20px;
+
+    color: #306292;
+
+    background: #FFFFFF;
+    border: 3px solid #386896;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .tag-selected {
+    border-color: #FA6D6C;
+  }
+`;
+
+export const ButtonBlock = styled.div`
+  margin-top: 60px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button<IButtonProps>`
+  height: 50px;
+  width: 150px;
+  border-radius: 15px;
+  border: 0;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.15);
+  outline: 0;
+
+  background-color: ${props => props.background};
+  transition: opacity 0.2s;
+
+  font-size: 18px;
+  font-weight: 700;
+  color: #FFF;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    opacity: 0.9;
+  }
 `;
