ee3ecfe26fe4bf2de8a74dd72a2375995b8c6f19
renansdf <rnn.frts@gmail.com>
2020-11-14 23:38:14 -0300

    new structure and styles to mimic Linker app login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import Menu from '../../components/Menu';
 import PhoneHeader from '../../components/PhoneHeader';
 
 import { Container } from './styles';
+import LinkerLogo from '../../images/linker-logo-branco.png';
+import { Link } from 'react-router-dom';
+
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <PhoneHeader>sign in</PhoneHeader>
-      <h1>tela de sign in</h1>
-      <Menu />
+      <PhoneHeader background="#316094">sign in</PhoneHeader>
+      <img src={LinkerLogo} alt="linker logo" />
+
+      <input type="text" placeholder="CPF" />
+      <Link to="/dashboard">Continuar</Link>
     </Container>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,58 @@
 import styled from 'styled-components';
 
+import loginBg from '../../images/login-bg.png';
+
 export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: url(${loginBg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  padding: 0 20px 40px;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+
+  img{
+    position: absolute;
+    top: 40%;
+    width: 50%;
+  }
+
+  input{
+    width: 100%;
+    background: none;
+    border: none;
+    border-bottom: 2px solid #fff;
+    padding-bottom: 3px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    color: #fff;
+
+    &::placeholder{
+      color: #fff;
+    }
+
+    &:focus{
+      outline: none;
+    }
+  }
+
+  a{
+    background: rgb(255 98 97 / 72%);
+    color: #fff;
+    text-decoration: none;
+    padding: 12px 10px;
+    border-radius: 30px;
+    font-size: 14px;
+    margin-top: 20px;
+    width: 100%;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 1px;
+
+  }
 `;
