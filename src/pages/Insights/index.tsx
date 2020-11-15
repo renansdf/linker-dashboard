d1cbaf88166016574a7328e5e49ed9ef1d992ffe
renansdf <rnn.frts@gmail.com>
2020-11-14 23:37:08 -0300

    page gets posts and texts conditionaly

--- src/pages/Insights/index.tsx
+++ src/pages/Insights/index.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
 import Menu from '../../components/Menu';
 import PhoneHeader from '../../components/PhoneHeader';
 
-import { Container } from './styles';
+import { Container, Post } from './styles';
+
+interface IPost {
+  id: number;
+  postUrl: string;
+  imgUrl: string;
+  title: string;
+  category: string;
+}
+
+interface IDica {
+  dicaText: string;
+}
 
 const Insights: React.FC = () => {
+  const [posts, setPosts] = useState<IPost[]>();
+  const [dica, setDica] = useState<IDica>();
+
+  useEffect(() => {
+    //get transactions
+    //check if profit this month is smaller
+
+    setDica({
+      dicaText: 'Seu lucro tem aumentado, parabéns!'
+    });
+
+    api.get('/posts?situation=positive-balance').then(response => {
+      setPosts(response.data);
+    });
+  }, []);
+
   return (
     <Container>
-      <PhoneHeader>insights</PhoneHeader>
-      <h1>Insights</h1>
-      <Menu />
+      <PhoneHeader background="#fcfcfe" color="#316094"><Menu menuBarColor="#316094" /><h1>dicas</h1></PhoneHeader>
+
+      <h2><span>{dica?.dicaText}</span><br />Abaixo algumas dicas pra esse momento da sua empresa.</h2>
+
+      {posts && posts.map(post => (
+        <Post key={post.id}>
+          <Link to={`/post/${post.id}`}>
+            <img src={post.imgUrl} alt={post.title} />
+            <div>
+              <strong>{post.category}</strong>
+              <h3>{post.title}</h3>
+            </div>
+          </Link>
+        </Post>
+      ))}
     </Container>
   );
 }
