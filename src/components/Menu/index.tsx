6b59ce9da4f5a19628150ac887c997904f945d4f
renansdf <rnn.frts@gmail.com>
2020-11-14 23:34:55 -0300

    updated menu to hamburguer with new states and styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiMenu } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, MenuIcon, MenuContainer } from './styles';
 
-const Menu: React.FC = () => {
+interface IMenuProps {
+  menuBarColor?: string;
+}
+
+const Menu: React.FC<IMenuProps> = ({ menuBarColor }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Container>
-      <Link to="/" >signin</Link>
-      <Link to="/dashboard" >dashboard</Link>
-      <Link to="/monitoramento" >monitoramento</Link>
-      <Link to="/insights" >insights</Link>
-      <Link to="/tag" >tag</Link>
+      <MenuIcon onClick={() => setIsOpen(!isOpen)}><FiMenu size={30} color={menuBarColor} /></MenuIcon>
+      <MenuContainer open={isOpen}>
+        <Link to="/" >signin</Link>
+        <Link to="/dashboard" >início</Link>
+        <Link to="/monitoramento" >monitoramento</Link>
+        <Link to="/insights" >dicas</Link>
+        <Link to="/tag" >tag</Link>
+      </MenuContainer>
     </Container>
   );
 }
